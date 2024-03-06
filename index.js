792d572fafc08701c0f6108831d70a410fe583bb
Luca Sofronie <luca.sofronie@yahoo.com>
2024-03-06 11:10:41 +0200

    a

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-
-async function modifyNumber(newNumber, email, password, id){
-  const puppeteer = require('puppeteer');
-  const browser = await puppeteer.launch({headless: false});
+async function modifyNumber(newNumber, email, password, id) {
+  const puppeteer = require("puppeteer");
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.setViewport({height: 1080, width: 1000});
+  await page.setViewport({ height: 1080, width: 1000 });
   await page.goto(`https://waapi.app/account/instances/${id}`);
 
   await page.evaluate(async () => {
-    await new Promise(resolve => setTimeout(resolve, 500));
+    await new Promise((resolve) => setTimeout(resolve, 500));
   });
 
-  await page.type('#email',email);
-  await page.type('#password',password);
+  await page.type("#email", email);
+  await page.type("#password", password);
   await page.click('button[type="submit"]');
 
   // await page.evaluate(async () => {
   //   await new Promise(resolve => setTimeout(resolve, 1000));
   // });
-  
+
   const textInputHTML = 'input[wire\\:model\\.live="phoneNumber"][type="text"]';
   await page.waitForSelector(textInputHTML);
 
@@ -27,23 +26,32 @@ async function modifyNumber(newNumber, email, password, id){
   //   await page.keyboard.press('Backspace');
 
   const input = await page.$(textInputHTML);
-  await input.click({ clickCount: 3 })
-  await page.keyboard.press('Backspace')
-    
+  console.log(input);
+  await page.evaluate(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  });
+  await input.click({ clickCount: 3 });
+
+  await page.evaluate(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  });
+
+  await page.keyboard.press("Backspace");
+
   await page.type(textInputHTML, newNumber);
-  
+
   const saveBtn = 'button[wire\\:click="save"]';
   await page.waitForSelector(saveBtn);
   await page.click(saveBtn);
   await page.click(saveBtn);
 
   await page.evaluate(async () => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
   });
 
   await browser.close();
 }
 
-module.exports = modifyNumber;
-
+module.exports = { modifyNumber };
+// modifyNumber("40735576793", "cogewed863@laymro.com", "whatsapp2024!", "6105");
 // modifyNumber('0768390157', 'cogewed863@laymro.com', 'whatsapp2024!', '5758');
